f691dffd5ddf00b234f303b371f66ba713185598
6hislain <ghislainmike80@gmail.com>
2023-11-27 16:59:24 +0200

    url & generic views

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("question/", views.QuestionList.as_view(), name="questions"),
+    path("question/<slug:slug>", views.QuestionDetail.as_view(), name="question"),
+    path("topic/<slug:slug>", views.TopicDetail.as_view(), name="topic"),
+    path("page/<slug:slug>", views.PageDetail.as_view(), name="page"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from typing import Any
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import ListView, DetailView
+from .models import Question, Answer, Topic, Page
+
 
 # Create your views here.
+class QuestionList(ListView):
+    paginate_by = 48
+    queryset = Question.objects.order_by("-created_at")
+    template_name = "question/index.html"
+
+
+class QuestionDetail(DetailView):
+    model = Question
+    paginate_by = 48
+    template_name = "question/detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        question = self.get_object()
+        answers = Answer.object.filter(question=question).order_by("-created_at")
+
+        paginator = Paginator(answers, self.paginate_by)
+        page = self.request.GET.get("page")
+
+        try:
+            paginated_answers = paginator.page(page)
+        except PageNotAnInteger:
+            paginated_answers = paginator.page(1)
+        except EmptyPage:
+            paginated_answers = paginator.page(paginator.num_pages)
+
+        context["answers"] = paginated_answers
+
+        return context
+
+
+class PageDetail(DetailView):
+    model = Page
+    template_name = "page/detail.html"
+
+
+class TopicDetail(DetailView):
+    model = Topic
+    paginate_by = 48
+    template_name = "topic/detail"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        topic = self.get_object()
+        questions = Question.object.filter(topic=topic).order_by("-created_at")
+
+        paginator = Paginator(questions, self.paginate_by)
+        page = self.request.GET.get("page")
+
+        try:
+            paginated_questions = paginator.page(page)
+        except PageNotAnInteger:
+            paginated_questions = paginator.page(1)
+        except EmptyPage:
+            paginated_questions = paginator.page(paginator.num_pages)
+
+        context["questions"] = paginated_questions
+
+        return context
