6ebbc147a59d2136737512aa2c8963dcd43b4b09
6hislain <ghislainmike80@gmail.com>
2024-03-22 14:25:08 +0200

    remove debug tool bar

--- forum/settings.py
+++ forum/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "tinymce",
     "core",
 ]
@@ -54,7 +53,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "forum.urls"
