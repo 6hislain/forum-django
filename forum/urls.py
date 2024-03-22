38186de412ce24556aa60358d7d951010b4d7039
6hislain <ghislainmike80@gmail.com>
2024-03-22 14:27:44 +0200

    remove debug url

--- forum/urls.py
+++ forum/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -27,5 +28,4 @@ urlpatterns = [
     path("", include("core.urls")),
     path("admin/", admin.site.urls),
     path("tinymce/", include("tinymce.urls")),
-    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
